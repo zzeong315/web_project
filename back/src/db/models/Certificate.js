5b413f3ba7ed142dcb1f9e52b7f1b32e1a0c7afc
siha <supersiha@gmail.com>
2022-08-29 15:34:03 +0900

    [add] certf error

--- back/src/db/models/Certificate.js
+++ back/src/db/models/Certificate.js
@@ -3,12 +3,18 @@ import { UserModel } from "../schemas/user";
 class Certificate {
   static async findById(userId) {
     const user = await UserModel.findOne({ userId });
+    if (!user) {
+      return new Error("user is not found");
+    }
     const certificates = user.certificates;
     return certificates;
   }
 
   static async create(userId, newCertificate) {
     const user = await UserModel.findOne({ userId });
+    if (!user) {
+      return new Error("user is not found");
+    }
     user.certificates.push(newCertificate);
     const createdNewCertificate = await user.save();
     return createdNewCertificate;
@@ -16,14 +22,22 @@ class Certificate {
 
   static async update(userId, certificateId, toUpdate) {
     const user = await UserModel.findOne({ id: userId });
+    if (!user) {
+      return new Error("user is not found");
+    }
     const certificates = user.certificates;
+    let flag = false;
     certificates.forEach((certificate) => {
       if (certificate._id.valueOf() === certificateId) {
         certificate.name = toUpdate.name;
         certificate.description = toUpdate.description;
         certificate.date = toUpdate.date;
+        flag = true;
       }
     });
+    if (!flag) {
+      return new Error("project is not found");
+    }
 
     const updatedCertificate = await user.save();
     return updatedCertificate;
@@ -31,6 +45,9 @@ class Certificate {
 
   static async delete(userId, certificateId) {
     let user = await UserModel.findOne({ id: userId });
+    if (!user) {
+      return new Error("user is not found");
+    }
     let certificates = user.certificates;
     user.certificates = certificates.filter(function (elem) {
       return elem._id.valueOf() !== certificateId;
