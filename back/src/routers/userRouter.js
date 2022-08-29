00b9b1c37f7b49f3caea8c876f8905ee29abab27
chaemin <chaemins193@gmail.com>
2022-08-29 15:08:14 +0900

    [fix] userRouter

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -74,9 +74,9 @@ userAuthRouter.get(
   async function (req, res, next) {
     try {
       // jwt토큰에서 추출된 사용자 id를 가지고 db에서 사용자 정보를 찾음.
-      const userId = req.userId;
+      const user_id = req.userId;
       const currentUserInfo = await userAuthService.getUserInfo({
-        userId,
+        user_id,
       });
 
       if (currentUserInfo.errorMessage) {
