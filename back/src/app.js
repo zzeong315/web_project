b489a35ee514d73245aef2831c5cabab2edb2acb
siha <supersiha@gmail.com>
2022-09-01 13:16:53 +0900

    [fix] upload

--- back/src/app.js
+++ back/src/app.js
@@ -69,8 +69,8 @@ app.post("/upload", login_required, upload.single("file"), (req, res, next) => {
     next(error);
   }
   res.status(201).send({
-    imgUrl: `http://localhost:5001/images/${req.file.filename}`,
-    // imgUrl: `http://kdt-ai5-team13.elicecoding.com:5001/images/${fileName}.${fileType[1]}`,
+    // imgUrl: `http://localhost:5001/images/${req.file.filename}`,
+    imgUrl: `http://kdt-ai5-team13.elicecoding.com:5001/images/${req.file.filename}`,
   });
 });
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
